e767e47d3a8f1ed0951c781a9e8b1a2b7c38938a
verbumfeit <verbumfeit@tuta.io>
2025-04-03 21:46:51 +0200

    dev stuff

--- ct/sabnzbd.sh
+++ ct/sabnzbd.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/verbumfeit/ProxmoxVE/refs/heads/add-par2cmdline-turbo/misc/build.func)
 # Copyright (c) 2021-2025 tteck
 # Author: tteck (tteckster)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
